342214c1251e4b0936add47c049965058e78d18f
Bryan Campbell <bryanjcampbell1@gmail.com>
2020-06-01 14:46:38 -0400

    css more consistent on home page

--- src/Home.js
+++ src/Home.js
@@ -89,70 +89,74 @@ class Home extends Component {
         return (
             <div>
                 <Row style={{marginTop:50}}>
-                    <Col></Col>
+                    <Col style={{display:'flex', justifyContent:'center'}}>
+                        <Card style={{width:500}}>
+                            <Card.Body style={{padding:40}}>
+                                <Form style={{marginTop:-15}}>
+                                    <Form.Group controlId="deposit">
+                                        <Form.Label style={{fontWeight:'bold', fontSize:16, color:'slate', }}>Deposit Amount</Form.Label>
+                                        <InputGroup className="mb-3">
+                                            <FormControl
+                                                placeholder="0"
+                                                aria-label="Deposit"
+                                                aria-describedby="basic-addon1"
+                                                onChange={(e)=> this.updateForm(e.target.id, e.target.value)}
+                                            />
+                                            <InputGroup.Append>
+                                                <InputGroup.Text id="basic-addon2">XTZ</InputGroup.Text>
+                                            </InputGroup.Append>
+                                        </InputGroup>
+                                    </Form.Group>
 
-                    <Col xs={6} >
+                                    <Form.Group controlId="duration" style={{marginTop:-10}}>
+                                        <Form.Label style={{fontWeight:'bold', fontSize:16, color:'slate'}}>Lockup Period</Form.Label>
+                                        <Form.Control as="select"
+                                                      onChange={(e)=> this.setReturnRate(e.target.value)}>
+                                            <option>3 Months</option>
+                                            <option>6 Months</option>
+                                            <option>9 Months</option>
+                                            <option>1 Year</option>
+                                            <option>15 Months</option>
+                                            <option>18 Months</option>
+                                            <option>21 Months</option>
+                                            <option>2 Years</option>
+                                        </Form.Control>
+                                    </Form.Group>
 
-                        <Card>
-                            <Card.Body style={{padding:50}}>
-                        <Form>
-                            <Form.Group as={Row} controlId="deposit">
-                                <Form.Label>Deposit Amount</Form.Label>
-                                <InputGroup className="mb-3">
-                                    <FormControl
-                                        placeholder="0"
-                                        aria-label="Deposit"
-                                        aria-describedby="basic-addon1"
-                                        onChange={(e)=> this.updateForm(e.target.id, e.target.value)}
-                                    />
-                                    <InputGroup.Append>
-                                        <InputGroup.Text id="basic-addon2">XTZ</InputGroup.Text>
-                                    </InputGroup.Append>
-                                </InputGroup>
-                            </Form.Group>
+                                    <Form.Group  controlId="currency" style={{marginTop:15}}>
+                                        <Form.Label style={{fontWeight:'bold', fontSize:16, color:'slate'}}>  Return Currency</Form.Label>
+                                        <Form.Control as="select"
+                                                      onChange={(e)=> this.updateForm(e.target.id, e.target.value)}>
+                                            <option>XTZ</option>
+                                            <option>USDtz</option>
+                                            <option>tzBTC</option>
+                                        </Form.Control>
+                                    </Form.Group>
 
-                            <Form.Group as={Row} controlId="duration" style={{marginTop:-20}}>
-                                <Form.Label>Lockup Period</Form.Label>
-                                <Form.Control as="select"
-                                              onChange={(e)=> this.setReturnRate(e.target.value)}>
-                                    <option>3 Months</option>
-                                    <option>6 Months</option>
-                                    <option>9 Months</option>
-                                    <option>1 Year</option>
-                                    <option>15 Months</option>
-                                    <option>18 Months</option>
-                                    <option>21 Months</option>
-                                    <option>2 Years</option>
-                                </Form.Control>
-                            </Form.Group>
+                                </Form>
+                                <div style={{display:'flex', justifyContent:'center', marginTop:30}}>
 
-                            <Form.Group as={Row} controlId="currency" style={{marginTop:10}}>
-                                <Form.Label>Return Currency</Form.Label>
-                                <Form.Control as="select"
-                                              onChange={(e)=> this.updateForm(e.target.id, e.target.value)}>
-                                    <option>XTZ</option>
-                                    <option>USDtz</option>
-                                    <option>tzBTC</option>
-                                </Form.Control>
-                            </Form.Group>
+                                    <Card style={{backgroundColor:'whitesmoke'}}>
+                                        <Card.Body>
 
-                        </Form>
-                        <div style={{display:'flex', justifyContent:'center', marginTop:35}}>
-                            <Card style={{backgroundColor:'lightblue', width:'70%', height:100}}>
-                                <p>Return</p>
-                                <div><p style={{display:'flex', justifyContent:'center'}}>{this.state.deposit*this.state.returnRate} {this.state.currency}</p></div>
-                            </Card>
-                        </div>
-                        <div style={{display:'flex', justifyContent:'center', marginTop:35}}>
-                            <Button size="lg" onClick={() => this.setState({previewModalShow: true}) }> Get Stake Now </Button>
-                        </div>
+                                            <p style={{fontWeight:'bold', fontSize:16, color:'slate'}}>
+                                                Return
+                                            </p>
+                                            <ul style={{fontSize:16, color:'slate', marginTop:-5}}>
+                                                <li> {this.state.deposit*this.state.returnRate} {this.state.currency} Stake </li>
+                                                <li> Certificate redeemable for 100 XTX on the date 9/11/22</li>
+                                            </ul>
+
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                                <div style={{display:'flex', justifyContent:'center', marginTop:30}}>
+                                    <Button size="lg" onClick={() => this.setState({previewModalShow: true}) }> Get Stake Now </Button>
+                                </div>
                             </Card.Body>
                         </Card>
                     </Col>
-
-                    <Col ></Col>
                 </Row>
-
                 <PreviewModal
                     show={this.state.previewModalShow}
                     onHide={() => this.hideModals()}
